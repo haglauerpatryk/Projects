51aacda4ddb87b62eaa002a43df4f5a013f73cb4
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-02-19 20:21:42 +0100

    Rearranged views.py
    Created StockView(BasView) class. That is parent to TablesView, ChartsView, FavouriteView to hold simmilar data.

--- stockproject/core/views.py
+++ stockproject/core/views.py
@@ -4,7 +4,18 @@ from django.contrib import messages
 from django.views import View
 
 
-# Create your views here.
+"""
+Showcase of the structure of the views.py file
+
+BaseView:
+    IndexView
+    ContactView
+    StockView:
+        TablesView
+        ChartsView
+        FavouriteView
+
+"""
 
 class BaseView(View):
     title = None
@@ -18,34 +29,54 @@ class IndexView(BaseView):
     title = "Dashboard"
     template_name = "index.html"
 
-
-class TablesView(BaseView):
-    title = "Tables"
-    template_name = "tables.html"
-
-
-class ChartsView(BaseView):
-    title = "Charts"
-    template_name = "charts.html"
-
-
-class FavouriteView(BaseView):
-    title = "Favourite"
-    template_name = "favourite.html"
-
-
 class ContactView(BaseView):
     title = "Contact"
     template_name = "contact.html"
 
     def post(self, request):
-        if request.method == "POST":
-            name    = request.POST.get("name")
-            email   = request.POST.get("email")
-            message = request.POST.get("message")
-            pnumber = request.POST.get("pnumber")
-
-            query = Contact(name=name, email=email, message=message, pnumber=pnumber)
-            query.save()
-            messages.info(request, "Your message has been sent successfully!")
+        if request.method != "POST":
             return render(request, "contact.html", {"title": self.title})
+        
+        name    = request.POST.get("name")
+        email   = request.POST.get("email")
+        message = request.POST.get("message")
+        pnumber = request.POST.get("pnumber")
+
+        query = Contact(name=name, email=email, message=message, pnumber=pnumber)
+        query.save()
+        messages.info(request, "Your message has been sent successfully!")
+        return render(request, "contact.html", {"title": self.title})
+    
+
+class StockView(BaseView):
+
+    def post(self, request):
+        if request.method != "POST":
+            return render(request, "index.html", {"title": self.title})
+
+        ticker = request.POST.get("ticker")
+        date   = request.POST.get("date")
+        open   = request.POST.get("open")
+        high   = request.POST.get("high")
+        low    = request.POST.get("low")
+        close  = request.POST.get("close")
+        volume = request.POST.get("volume")
+
+        query = Stock(ticker=ticker, date=date, open=open, high=high, low=low, close=close, volume=volume)
+        query.save()
+        messages.info(request, "Your stock has been saved successfully!")
+        return render(request, "index.html", {"title": self.title})
+
+class TablesView(StockView):
+    title = "Tables"
+    template_name = "tables.html"
+
+
+class ChartsView(StockView):
+    title = "Charts"
+    template_name = "charts.html"
+
+
+class FavouriteView(StockView):
+    title = "Favourite"
+    template_name = "favourite.html"
