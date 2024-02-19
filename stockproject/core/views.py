7548371c1e98d4311536a84e1be2e4936ba00272
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-02-19 17:42:02 +0100

    Added Contact form to templates, expanded it in views. Changed block title to variable.
    I've added in /core/views.py to each function a title variable that is called out in base.html. Removed block from other files accordingly.
    Added form to contact.html and made connection to DB in views.py

--- stockproject/core/views.py
+++ stockproject/core/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
+from .models import Stock, Contact
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    title = "Dashboard"
+
+    return render(request, "index.html", {"title": title})
 
 def tables(request):
-    return render(request, "tables.html")
+    title = "Tables"
+
+    return render(request, "tables.html", {"title": title})
 
 def charts(request):
-    return render(request, "charts.html")
+    title = "Charts"
+
+    return render(request, "charts.html", {"title": title})
 
 def favourite(request):
-    return render(request, "favourite.html")
+    title = "Favourite"
+
+    return render(request, "favourite.html", {"title": title})
 
 def contact(request):
-    return render(request, "contact.html")
+    title = "Contact"
+
+    if request.method == "POST":
+
+        name    = request.POST.get("name")
+        email   = request.POST.get("email")
+        message = request.POST.get("message")
+        pnumber = request.POST.get("pnumber")
+
+        query = Contact(name=name, email=email, message=message, pnumber=pnumber)
+        query.save()
+        messages.info(request, "Your message has been sent successfully!")
+        return render(request, "contact.html", {"title": title})
+
+    return render(request, "contact.html", {"title": title})
