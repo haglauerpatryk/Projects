cb4355686f6cf1c86381975f710a02d23d3a568f
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-02-19 16:41:18 +0100

    Expanded models, added them to admin.
    Added model Contact and Stock.
    Added both of them to admin.

--- stockproject/core/admin.py
+++ stockproject/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Stock, Contact
 
 # Register your models here.
+
+admin.site.register(Stock)
+admin.site.register(Contact)
--- stockproject/core/models.py
+++ stockproject/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    
+    ticker = models.CharField(max_length=10)
+    date   = models.DateField()
+    open   = models.FloatField()
+    high   = models.FloatField()
+    low    = models.FloatField()
+    close  = models.FloatField()
+    volume = models.FloatField()
+
+    def __str__(self):
+        return self.ticker
+    
+
+class Contact(models.Model):
+    
+    name    = models.CharField(max_length=200)
+    email   = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
