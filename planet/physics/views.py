11e6dd1c00a6286f922e1b99a4f60e00ab5f24a0
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-02-25 20:25:59 +0100

    Creating Foundations for a Project
    Added graph in three.js
    Created class Physics.

--- planet/physics/views.py
+++ planet/physics/views.py
@@ -1,7 +1,29 @@
+import numpy as np
+
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
+class Index(View):
+    title = "Physics"
+    template_name = "index.html"
 
-def index(request):
-    return render(request, 'index.html')
+    def get(self, request):
+        if request.method == "GET":
+            return render(request, self.template_name, {"title": self.title})
+        
+
+class Physics():
+    r"""
+    For the naming of the methods 
+    I'll use names of the laws of physics.
+
+    For the naming of the variables I'll use
+    the names of the variables in the laws 
+    of physics and corresponding letters.
+    """
+
+    def newton_first_law(self, variable):
+        pass
+    
